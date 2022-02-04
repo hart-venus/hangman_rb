4c5b242307842c9601148cc7d0718871e38498d1
Ariel Leyva <aleyva1509@gmail.com>
2022-02-04 17:00:33 -0600

    random word logic done

--- lib/game.rb
+++ lib/game.rb
@@ -4,9 +4,11 @@ word_list_file = File.open('../google-10000-english-no-swears.txt')
 word_list = word_list_file.readlines
 word_list_filtered = word_list.select { |word| word.length > 5 && word.length < 12 }
 
-def start_game
-  puts 'hi'
+def start_game(word_list)
+  guesses_left = 10
+  random_word = word_list.sample()
+  puts random_word
 end
 
 puts 'Welcome to Hangman! type "start" to start the game.'
-start_game if gets.chomp == 'start'
+start_game(word_list_filtered) if gets.chomp == 'start'
