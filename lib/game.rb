d8d57adc1a1083c71a1cb85924401975b23a5f59
Ariel Leyva <aleyva1509@gmail.com>
2022-02-04 16:45:43 -0600

    bug fixed hell yeah

--- lib/game.rb
+++ lib/game.rb
@@ -2,5 +2,5 @@
 
 word_list_file = File.open('../google-10000-english-no-swears.txt')
 word_list = word_list_file.readlines
-word_list_filtered = word_list.select{|word| word.length>5 && word.length<12}
+word_list_filtered = word_list.select { |word| word.length > 5 && word.length < 12 }
 puts word_list_filtered
