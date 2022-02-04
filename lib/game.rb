0b87ff940abcaf003dfde78a2617e8afe0b55e51
Ariel Leyva <aleyva1509@gmail.com>
2022-02-04 17:16:27 -0600

    basic function almost complete

--- lib/game.rb
+++ lib/game.rb
@@ -6,8 +6,13 @@ word_list_filtered = word_list.select { |word| word.length > 5 && word.length < 
 
 def start_game(word_list)
   guesses_left = 10
-  random_word = word_list.sample()
-  puts random_word
+  word_guessed = false
+  random_word = word_list.sample
+  letter_guess_array = Array.new(random_word.length, '_')
+  until word_guessed || guesses_left.zero?
+    puts letter_guess_array.join(' ')
+    guesses_left -= 1
+  end
 end
 
 puts 'Welcome to Hangman! type "start" to start the game.'
