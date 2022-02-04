f5221e80e8963a2f520434a7e33b28d7cd374c6f
Ariel Leyva <aleyva1509@gmail.com>
2022-02-04 16:40:27 -0600

    weird sync error

--- lib/game.rb
+++ lib/game.rb
@@ -1,3 +1,6 @@
+# frozen_string_literal: true
+
 word_list_file = File.open('../google-10000-english-no-swears.txt')
 word_list = word_list_file.readlines
-puts word_list
+word_list_filtered = word_list.select{|word| word.length>5 && word.length<12}
+puts word_list_filtered
