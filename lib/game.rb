10509ecbe4e32eebc6c37757c77f74f78be3d1b7
Ariel Leyva <aleyva1509@gmail.com>
2022-02-04 16:55:38 -0600

    basic 'start game' input

--- lib/game.rb
+++ lib/game.rb
@@ -3,4 +3,10 @@
 word_list_file = File.open('../google-10000-english-no-swears.txt')
 word_list = word_list_file.readlines
 word_list_filtered = word_list.select { |word| word.length > 5 && word.length < 12 }
-puts word_list_filtered
+
+def start_game
+  puts 'hi'
+end
+
+puts 'Welcome to Hangman! type "start" to start the game.'
+start_game if gets.chomp == 'start'
