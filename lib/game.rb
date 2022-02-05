23a02897b3c0be49f127e8bbbbf91042357efc78
Ariel Leyva <aleyva1509@gmail.com>
2022-02-05 16:56:31 -0600

    finished :))))

--- lib/game.rb
+++ lib/game.rb
@@ -1,33 +1,43 @@
 # frozen_string_literal: true
+
 require 'json'
 word_list_file = File.open('../google-10000-english-no-swears.txt')
 word_list = word_list_file.readlines
 word_list_filtered = word_list.select { |word| word.length > 5 && word.length < 12 }
 
-def save(guessed_letters, letter_guess_array, random_word)
+def save(guessed_letters, letter_guess_array, guesses_left, random_word)
   save_hash = {
-    guesses:guessed_letters,
-    array:letter_guess_array,
-    word:random_word
+    guesses: guessed_letters,
+    guesses_left: guesses_left,
+    array: letter_guess_array,
+    word: random_word
   }
-  File.open('save.json', 'w'){|f| f.write(save_hash.to_json)}
+  File.open('save.json', 'w') { |f| f.write(save_hash.to_json) }
+  puts 'file saved successfully.'
   # puts "#{guessed_letters} #{letter_guess_array}, #{random_word}"
 end
 
-def start_game(word_list)
+def start_game(word_list, saved_game=false)
   guesses_left = 10
   word_guessed = false
   guessed_letters = []
-  random_word = word_list.sample
-  letter_guess_array = Array.new(random_word.length - 1, '_')
+  random_word = word_list.sample unless saved_game
+  letter_guess_array = Array.new(random_word.length - 1, '_') unless saved_game
 
+  if saved_game
+    guesses_left = saved_game['guesses_left']
+    guessed_letters = saved_game['guesses']
+    letter_guess_array = saved_game['array']
+    random_word = saved_game['word']
+  end
+  
   until word_guessed || guesses_left.zero?
     puts letter_guess_array.join(' ')
     puts "Incorrect guesses left: #{guesses_left}"
     puts "Guessed letters: #{guessed_letters}"
     input = gets.chomp.downcase
     if input == 'save'
-      save(guessed_letters, letter_guess_array, random_word) 
+      save(guessed_letters, letter_guess_array, guesses_left, random_word)
       break
     end
     guessed_letter = input[0]
@@ -70,5 +80,14 @@ def check_letter(letter_guess_array, guessed_letter, random_word)
   return_value
 end
 
-puts 'Welcome to Hangman! type "start" to start the game. Type "save" anytime to save the game.'
-start_game(word_list_filtered) if gets.chomp == 'start'
+def load_game
+  savefile = File.read('save.json')
+  saved_hash = JSON.parse(savefile) || 'Invalid file.'
+  start_game([], saved_hash)
+end
+
+puts 'Welcome to Hangman! type "start" to start, or "load" to load a previous game. Type "save" anytime to save.'
+
+input = gets.chomp.downcase
+start_game(word_list_filtered) if input == 'start'
+load_game if input == 'load'
